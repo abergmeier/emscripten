27978af63a9089fc7974badd419fb88e8fed8b0c
Andreas Bergmeier <abergmeier@gmx.net>
2013-12-23 10:19:15 +0100

    Accept multiple entries for INCLUDE_DIRS and LIBRARIES when detecting FreeImage.

--- cmake/Modules/FindFreeImage.cmake
+++ cmake/Modules/FindFreeImage.cmake
@@ -11,25 +11,30 @@ pkg_check_modules( FREEIMAGE QUIET freeimage )
 
 set( FREEIMAGE_DEFINITIONS ${FREEIMAGE_CFLAGS_OTHER} )
 
-find_path( FREEIMAGE_INCLUDE_DIR
-	NAMES FreeImage.h
-	HINTS ${FREEIMAGE_INCLUDEDIR} ${FREEIMAGE_INCLUDE_DIRS}
-)
+set( FREEIMAGE_LIBRARY_LIST "" )
+set( index 0 )
 
-find_library( FREEIMAGE_LIBRARY
-	NAMES ${FREEIMAGE_LIBRARIES}
-	HINTS ${FREEIMAGE_LIBDIR} ${FREEIMAGE_LIBRARY_DIRS}
-)
+foreach( FREEIMAGE_LIB ${FREEIMAGE_LIBRARIES} )
 
-set( FREEIMAGE_LIBRARIES    ${FREEIMAGE_LIBRARY}     )
-set( FREEIMAGE_INCLUDE_DIRS ${FREEIMAGE_INCLUDE_DIR} )
+	# FIXME: need find_library with different variables,
+	# since it works only for the first call
+	find_library( FREEIMAGE_LIBRARY${index}
+		NAMES ${FREEIMAGE_LIB}
+		HINTS ${FREEIMAGE_LIBDIR} ${FREEIMAGE_LIBRARY_DIRS}
+	)
+
+	list( APPEND FREEIMAGE_LIBRARY_LIST "${FREEIMAGE_LIBRARY${index}}" )
+	MATH(EXPR index "${index}+1")
+endforeach()
+
+set( FREEIMAGE_LIBRARIES ${FREEIMAGE_LIBRARY_LIST} )
 
 include( FindPackageHandleStandardArgs )
 
 # handle the QUIETLY and REQUIRED arguments and set FREEIMAGE_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args( FreeImage DEFAULT_MSG
-	FREEIMAGE_LIBRARY FREEIMAGE_INCLUDE_DIR
+	FREEIMAGE_LIBRARY_LIST FREEIMAGE_INCLUDE_DIRS
 )
 
 mark_as_advanced( FREEIMAGE_INCLUDE_DIR FREEIMAGE_LIBRARY )
